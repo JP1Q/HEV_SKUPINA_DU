8092f0d23ec136724549407e93e29aa3ef8f79cb
Aroteo <46629197+JP1Q@users.noreply.github.com>
2023-11-22 20:52:26 +0100

    Basic Hrac a Zed

---
+++ Assets/ScriptyHonza/Scripts/PlayerScript.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    [SerializeField]
+    Rigidbody Playerrb;
+
+    [SerializeField]
+    float PlayerSpeed;
+
+
+    private Vector3 direction;
+    // Start is called before the first frame update
+    void Start()
+    {
+        direction = transform.forward;   // Inicializace smeru chuze <3
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Playerrb.AddForce(direction * PlayerSpeed * Time.deltaTime); // delani chuze
+    }
+
+    void OnCollisionEnter(Collision collision){
+        if(collision.gameObject.tag == "TurnRightWall"){
+
+            if(direction == transform.right){
+                direction = -transform.forward;
+            }else if(direction == -transform.forward){
+                direction = -transform.right;
+            }else if(direction == -transform.right){
+                direction = transform.forward;
+            }else if(direction == transform.forward){
+                direction = transform.right;
+            }
+
+
+            Debug.Log("There's second plane incoming");
+
+        }
+
+
+    }
+    
+}
