89afc0e4787219894bfb0273e71280bf3515d2c2
Edatorcz <nietsch.jakub@gmail.com>
2023-12-05 13:08:23 +0100

    Menu finished
    v kódu odstranit komentář, stěny nemají kolize

--- Assets/VeciNietsch/Scripts/LevelSelector.cs
+++ Assets/VeciNietsch/Scripts/LevelSelector.cs
@@ -14,6 +14,18 @@ public class LevelSelector : MonoBehaviour
 
     public void SelectedScene(int Level)
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        if (Level == 1)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+        else if (Level == 2)
+        {
+            Debug.Log("Level 2");
+            //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+        }
+        else
+        {
+            Debug.Log("Level 3");
+        }
     }
 }
